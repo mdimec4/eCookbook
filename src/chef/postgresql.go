ca5babcef7bd510c54af07cee32e7a8303309c7b
Miha Dimec <miha.dimec@visionect.com>
2017-07-04 17:18:05 +0200

    implemented majority of REST api in GO

--- src/chef/postgresql.go
+++ src/chef/postgresql.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-
 	"database/sql"
 	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
 	_ "github.com/lib/pq"
 )
 
 type PostgresBackend struct {
-	dsn           string
-	Database      *sql.DB
+	dsn      string
+	Database *sql.DB
 }
-
-
 
 func NewPostgresConnection(dataSourceName string) (*PostgresBackend, error) {
 	var err error
@@ -26,42 +27,57 @@ func NewPostgresConnection(dataSourceName string) (*PostgresBackend, error) {
 	rows, err := postgresDB.Database.Query("SELECT data FROM recepie LIMIT 1")
 	if err != nil {
 		_, err = postgresDB.Database.Exec(
-            "CREATE TABLE recepie (recepie_id text UNIQUE, data jsonb)")
+			"CREATE TABLE recepie (recepie_id text PRIMARY KEY, data jsonb)")
 		if err != nil {
 			return nil, err
 		}
 	} else {
 		rows.Close()
 	}
-    return postgresDB, nil
+	return postgresDB, nil
 }
 
+/*
+double insert should yeald this:
+ERROR:  duplicate key value violates unique constraint "recepie_recepie_id_key"
+DETAIL:  Key (recepie_id)=(333) already exists.
+*/
 func (postgres *PostgresBackend) CreateRecepie(r Recepie) error {
 	id := r.RecepieID
 	recJson, err := json.Marshal(r)
-		if err != nil {
+	if err != nil {
 		return err
 	}
 	_, err = postgres.Database.Exec(
 		"INSERT INTO recepie (recepie_id, data) VALUES ($1, $2)",
-	    id, string(recJson))
+		id, string(recJson))
 	if err != nil {
+		if strings.Contains(err.Error(), "duplicate key value violates unique constraint") {
+			fmt.Fprintf(os.Stderr, "CreateRecepie(): %s\n", err)
+			return errRecepieExist
+		}
 		return err
 	}
 	return nil
 }
 
 func (postgres *PostgresBackend) Update(r Recepie) error {
+	var rs sql.Result
 	id := r.RecepieID
 	recJson, err := json.Marshal(r)
-		if err != nil {
-		return err
-	}
-	_, err = postgres.Database.Exec(
-		"UPDATE recepie SET data = $2 WHERE recepie_id = $1",
-	    id, string(recJson))
 	if err != nil {
 		return err
+	}
+	rs, err = postgres.Database.Exec(
+		"UPDATE recepie SET data = $2 WHERE recepie_id = $1",
+		id, string(recJson))
+	if err != nil {
+		return err
+	}
+	if cnt, err := rs.RowsAffected(); err != nil {
+		return err
+	} else if cnt < 1 {
+		return errRecepieExistNot
 	}
 	return nil
 }
@@ -69,13 +85,13 @@ func (postgres *PostgresBackend) Update(r Recepie) error {
 func (postgres *PostgresBackend) UpdateOrCreateRecepie(r Recepie) error {
 	id := r.RecepieID
 	recJson, err := json.Marshal(r)
-		if err != nil {
+	if err != nil {
 		return err
 	}
 	_, err = postgres.Database.Exec(`
 		INSERT INTO recepie (recepie_id, data) VALUES ($1, $2)
 		ON CONFLICT (recepie_id) DO UPDATE SET data = excluded.data`,
-	    id, string(recJson))
+		id, string(recJson))
 	if err != nil {
 		return err
 	}
@@ -84,8 +100,8 @@ func (postgres *PostgresBackend) UpdateOrCreateRecepie(r Recepie) error {
 
 func (postgres *PostgresBackend) ListRecepies() ([]Recepie, error) {
 	var (
-		data string
-		recepie Recepie
+		data     string
+		recepie  Recepie
 		recepies []Recepie
 	)
 	rows, err := postgres.Database.Query("SELECT data FROM recepie")
@@ -99,6 +115,9 @@ func (postgres *PostgresBackend) ListRecepies() ([]Recepie, error) {
 	for rows.Next() {
 		err = rows.Scan(&data)
 		if err != nil {
+			if err == sql.ErrNoRows {
+				return []Recepie{}, nil
+			}
 			return nil, err
 		}
 		err = json.Unmarshal([]byte(data), &recepie)
@@ -112,14 +131,17 @@ func (postgres *PostgresBackend) ListRecepies() ([]Recepie, error) {
 
 func (postgres *PostgresBackend) GetRecepie(recepieID string) (Recepie, error) {
 	var (
-		data string
+		data    string
 		recepie Recepie
 	)
 	row := postgres.Database.QueryRow(
-			"SELECT data FROM recepie WHERE recepie_id = $1",
- 			recepieID)
-    err := row.Scan(&data)
+		"SELECT data FROM recepie WHERE recepie_id = $1",
+		recepieID)
+	err := row.Scan(&data)
 	if err != nil {
+		if err == sql.ErrNoRows {
+			err = errRecepieExistNot
+		}
 		return Recepie{}, err
 	}
 	err = json.Unmarshal([]byte(data), &recepie)
@@ -131,12 +153,16 @@ func (postgres *PostgresBackend) GetRecepie(recepieID string) (Recepie, error) {
 }
 
 func (postgres *PostgresBackend) Delete(recepieID string) error {
-	_, err := postgres.Database.Exec(
+	rs, err := postgres.Database.Exec(
 		"DELETE FROM recepie WHERE recepie_id = $1",
 		recepieID)
 	if err != nil {
 		return err
 	}
+	if cnt, err := rs.RowsAffected(); err != nil {
+		return err
+	} else if cnt < 1 {
+		return errRecepieExistNot
+	}
 	return nil
 }
-
--- src/chef/recepie.go
+++ src/chef/recepie.go
@@ -1,78 +1,195 @@
 package main
+
 import (
-    "fmt"
+	"crypto/rand"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"hash/crc32"
 	"net/http"
-    "os"
-    "hash/crc32"
-    "errors"
-    "crypto/rand"
+	"os"
+	//"strings"
+	//"unicode"
+
+	"github.com/gorilla/mux"
 )
 
+/*
+REST API guides
+- When should we use PUT and when should we use POST?
+  http://restcookbook.com/HTTP%20Methods/put-vs-post/
+- HTTP Status Codes
+  http://www.restapitutorial.com/httpstatuscodes.html
+- Rest api in GO sample
+  https://www.thepolyglotdeveloper.com/2016/07/create-a-simple-restful-api-with-golang/
+*/
+
 type Recepie struct {
-	RecepieID string `json:"recepie_id"`
-	Publisher string `json:"publisher"`
-	SourceURL string `json:"source_url"`
-	Title string `json:"title"`
-	ImageURL string `json:"image_url"`
-	Tags []string `json:"tags"`
-	Ingredients []string `json:"ingredients"`
+	RecepieID    string   `json:"recepie_id"`
+	Publisher    string   `json:"publisher"`
+	SourceURL    string   `json:"source_url"`
+	Title        string   `json:"title"`
+	ImageURL     string   `json:"image_url"`
+	Tags         []string `json:"tags"`
+	Ingredients  []string `json:"ingredients"`
 	Instructions []struct {
-		Number int `json:"number"`
-		ImageURL string `json:"image_url"`
+		Number      int    `json:"number"`
+		ImageURL    string `json:"image_url"`
 		Instruction string `json:"instruction"`
 	} `json:"instructions"`
 	Tips []string `json:"tips"`
 }
 
 var (
-    postgres *PostgresBackend
+	postgres *PostgresBackend
+)
+
+var (
+	errRecepieExist    error = errors.New("recipe already exists")
+	errRecepieExistNot error = errors.New("recipe doesn't exists")
 )
 
 func getRecepie(recepieID string) (Recepie, error) {
-    return postgres.GetRecepie(recepieID)
+	return postgres.GetRecepie(recepieID)
 }
 
 func getRecepieList() ([]Recepie, error) {
-    return postgres.ListRecepies()
+	return postgres.ListRecepies()
 }
 
 func update(recepie Recepie) error {
-    return postgres.Update(recepie)
+	return postgres.Update(recepie)
 }
 
+/*
+func whiteSpaceMap(str string) string {
+	return strings.Map(func(r rune) rune {
+		if unicode.IsSpace(r) {
+			return -1
+		}
+		return r
+	}, str)
+}
+*/
+
 func newRecepie(recepie Recepie) (string, error) {
-    if recepie.Title == "" {
-        return "", errors.New("Title is not set")
-    }
-    if recepie.RecepieID == "" {
-        var crc1 uint32 = ChecksumIEEE([]byte(recepie.Title))
-        var bArr []byte = make([]byte, 10)
-        rand.Read(bArr)
-        var crc2 uint32 = ChecksumIEEEstring(bArr)
-        recepie.RecepieID = fmt.Sprintf("%s-%s", crc1, crc2)
-    }
-    err := postgres.CreateRecepie(recepie)
-    return recepie.RecepieID, err
+	if recepie.Title == "" {
+		return "", errors.New("Title is not set")
+	}
+	if recepie.RecepieID == "" {
+		var crc1 uint32 = crc32.ChecksumIEEE([]byte(recepie.Title))
+		var bArr []byte = make([]byte, 4)
+		rand.Read(bArr)
+		var num32 uint32 = 0
+		num32 = uint32(bArr[3])<<24 | uint32(bArr[2])<<16 | uint32(bArr[1])<<8 | uint32(bArr[0])
+		recepie.RecepieID = fmt.Sprintf("%x-%x", crc1, num32)
+	}
+	err := postgres.CreateRecepie(recepie)
+	return recepie.RecepieID, err
+}
+
+func deleteRecepie(id string) error {
+	return postgres.Delete(id)
 }
 
 func GetRecepiesList(w http.ResponseWriter, r *http.Request) {
+	l, err := getRecepieList()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	b, err := json.Marshal(l)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println("str ", string(b))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
 }
 
 func GetRecepie(w http.ResponseWriter, r *http.Request) {
 }
 
 func PostNewRecepie(w http.ResponseWriter, r *http.Request) {
+	var recepie Recepie
+	err := json.NewDecoder(r.Body).Decode(&recepie)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	id, err := newRecepie(recepie)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//201 Created
+	//Location
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Location", r.URL.Path+"/"+id)
+	w.WriteHeader(http.StatusCreated)
 }
 
+func PutUpdateRecepie(w http.ResponseWriter, r *http.Request) {
+	var recepie Recepie
+	err := json.NewDecoder(r.Body).Decode(&recepie)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	params := mux.Vars(r)
+	if params["id"] != recepie.RecepieID {
+		http.Error(w, "URL vs. JSON body 'RecepieID' mismatch", http.StatusConflict)
+		return
+	}
+
+	err = update(recepie)
+	if err != nil {
+		if err == errRecepieExistNot {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//201 Created
+	//Location
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Location", r.URL.Path)
+	w.WriteHeader(http.StatusCreated)
+}
+
+func DeleteRecepie(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	err := deleteRecepie(params["id"])
+	if err != nil {
+		if err == errRecepieExistNot {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	//204 NoContent
+	w.WriteHeader(http.StatusNoContent)
+}
 
 func main() {
-    var (
-        err error
-    )
-    postgres, err = NewPostgresConnection(fmt.Sprintf("user=%s dbname=%s password=%s sslmode=disable host=%s port=%s",
-                                  "chef", "cookbook", "chef", "localhost", "5432"))
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "%s", err)
-        os.Exit(1)
-    }
+	var (
+		err error
+	)
+	postgres, err = NewPostgresConnection(fmt.Sprintf("user=%s dbname=%s password=%s sslmode=disable host=%s port=%s", "chef", "cookbook", "chef", "localhost", "5432")) //TODO use config.json file
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Setting database connection: %s\n", err)
+		os.Exit(1)
+	}
+	router := mux.NewRouter()
+	router.HandleFunc("/api/recepies", GetRecepiesList).Methods("GET")
+	router.HandleFunc("/api/recepies/{id}", GetRecepie).Methods("GET")
+	router.HandleFunc("/api/recepies", PostNewRecepie).Methods("POST")
+	router.HandleFunc("/api/recepies/{id}", PutUpdateRecepie).Methods("PUT")
+	router.HandleFunc("/api/recepies/{id}", DeleteRecepie).Methods("DELETE")
+	fmt.Fprintf(os.Stderr, "%v\n", http.ListenAndServe(":4006", router)) //TODO setup from config.json
 }
