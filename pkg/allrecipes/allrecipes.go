2ee2c019404e11a5d31fcbcd0dff90b58d6dd862
Miha Dimec <miha.dimec@visionect.com>
2018-02-16 08:52:09 +0100

    SomeTests

--- pkg/allrecipes/allrecipes.go
+++ pkg/allrecipes/allrecipes.go
@@ -3,24 +3,50 @@ package main
 import (
 	"fmt"
 	"golang.org/x/net/html" //https://stackoverflow.com/questions/30109061/golang-parse-html-extract-all-content-with-body-body-tags
-	"io/ioutil"
+	//"io/ioutil"
+	"golang.org/x/net/html/atom"
+	"net/http"
 )
 
 func getRecipe() (*html.Node, error) {
 	resp, err := http.Get("http://allrecipes.com/recipe/231495/texas-boiled-beer-shrimp/")
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	//body, err := ioutil.ReadAll(resp.Body)
 	return html.Parse(resp.Body)
 }
 
+func loopChildren(parentNode *html.Node, f func(c *html.Node) bool) {
+	for c := parentNode.FirstChild; c != nil; c = c.NextSibling {
+		if !f(c) {
+			break
+		}
+	}
+}
+
 func main() {
-	html, err := getRecipe()
+	// var ingredients []string
+	documentNode, err := getRecipe()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(html)
+	htmlNode := documentNode.FirstChild
+
+	//find body
+	var bodyNode *html.Node
+	loopChildren(htmlNode.NextSibling, func(n *html.Node) bool {
+		if n.DataAtom == atom.Body {
+			bodyNode = n
+			return false
+		}
+		return true
+	})
+
+	loopChildren(bodyNode, func(n *html.Node) bool {
+		fmt.Println(n.DataAtom, n)
+		return true
+	})
 }
