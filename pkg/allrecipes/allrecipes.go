bdbcbe9bfa41d3ea5c5bec51d69fcc1908def170
Miha Dimec <miha.dimec@visionect.com>
2018-02-16 12:25:30 +0100

    pkg/allrecepies: implemented html tree crawler

--- pkg/allrecipes/allrecipes.go
+++ pkg/allrecipes/allrecipes.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"golang.org/x/net/html" //https://stackoverflow.com/questions/30109061/golang-parse-html-extract-all-content-with-body-body-tags
 	//"io/ioutil"
-	"golang.org/x/net/html/atom"
+	//"golang.org/x/net/html/atom"
 	"net/http"
 )
 
@@ -20,6 +20,7 @@ func getRecipe() (*html.Node, error) {
 
 func loopSiblings(n *html.Node, f func(c *html.Node) bool) {
 	for c := n; c != nil; c = c.NextSibling {
+		fmt.Println("ls")
 		if !f(c) {
 			break
 		}
@@ -27,11 +28,29 @@ func loopSiblings(n *html.Node, f func(c *html.Node) bool) {
 }
 
 func loopChildren(n *html.Node, f func(c *html.Node) bool) {
-	for c := n; c != nil; c = c.FirstChild {
+	for c := n.FirstChild; c != nil; c = c.FirstChild {
+		fmt.Println("lc")
 		if !f(c) {
 			break
 		}
 	}
+}
+
+func crawlNodes(n *html.Node, f func(c *html.Node) bool) {
+	cont := true
+	fmt.Println(0)
+	loopSiblings(n, func(ns *html.Node) bool {
+		fmt.Println(1)
+		loopChildren(ns, func(cn *html.Node) bool {
+			fmt.Println(2)
+			cont = f(cn)
+			if cont {
+				crawlNodes(cn, f)
+			}
+			return cont
+		})
+		return cont
+	})
 }
 
 func main() {
@@ -41,20 +60,29 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	htmlNode := documentNode.FirstChild
+	/*
+		htmlNode := documentNode.FirstChild
 
-	//find body
-	var bodyNode *html.Node
-	loopSiblings(htmlNode.NextSibling.FirstChild, func(n *html.Node) bool {
-		if n.DataAtom == atom.Body {
-			bodyNode = n
-			return false
+		//find body
+		var bodyNode *html.Node
+		loopSiblings(htmlNode.NextSibling.FirstChild, func(n *html.Node) bool {
+			if n.DataAtom == atom.Body {
+				bodyNode = n
+				return false
+			}
+			return true
+		})
+
+		loopSiblings(bodyNode.FirstChild, func(n *html.Node) bool {
+			fmt.Println(n.DataAtom, n)
+			return true
+		})
+	*/
+	crawlNodes(documentNode, func(n *html.Node) bool {
+		if n == nil {
+			fmt.Println(n, " it is nil")
 		}
-		return true
-	})
-
-	loopSiblings(bodyNode.FirstChild, func(n *html.Node) bool {
-		fmt.Println(n.DataAtom, n)
+		fmt.Println(n)
 		return true
 	})
 }
